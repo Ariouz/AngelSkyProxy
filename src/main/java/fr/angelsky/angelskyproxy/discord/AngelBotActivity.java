11bf2a1c256d5ad934dca4b991659988f4bfcbd1
Ariouz <Ariouz@users.noreply.github.com>
2024-04-11 14:27:03 +0200

    Bot and plugin init

--- src/main/java/fr/angelsky/angelskyproxy/discord/AngelBotActivity.java
+++ src/main/java/fr/angelsky/angelskyproxy/discord/AngelBotActivity.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AngelBotActivity {
 
@@ -19,8 +20,12 @@ public class AngelBotActivity {
 
     public void activityTask()
     {
+        AtomicInteger integer = new AtomicInteger();
         angelSkyProxy.getServer().getScheduler().buildTask(angelSkyProxy, () -> {
-            jda.getPresence().setActivity(Activity.watching(angelSkyProxy.getServer().getPlayerCount() + " joueur" + (angelSkyProxy.getServer().getPlayerCount() > 1 ? "s" : "")));
+            if (integer.getAndAdd(1) % 2 == 0)
+                jda.getPresence().setActivity(Activity.watching(angelSkyProxy.getServer().getPlayerCount() + " joueur" + (angelSkyProxy.getServer().getPlayerCount() > 1 ? "s" : "")));
+            else
+                jda.getPresence().setActivity(Activity.playing("play.angelsky.fr"));
         }).repeat(30, TimeUnit.SECONDS).schedule();
     }
 
